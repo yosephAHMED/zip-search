a039294d9c73ba7b3e848fd7c8ec3599d0613e14
YosephAhmed <yosephahmed@aol.com>
2020-06-04 15:57:53 -0400

    Added user input field instead of hard-coding props. Added conditional rendering in componentDidUpdate so that it only executes axios.get when input is 5 characters. Added an event handler to change state.

--- src/components/ZipSearch.js
+++ src/components/ZipSearch.js
@@ -2,51 +2,78 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class ZipSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { zip: null };
+    constructor() {
+        super();
+        this.state = {
+            // variables that we will assign from API
+            zipCode: "",
+            stateName: "",
+            cityName: "",
+            locationText: "",
+            latitude: "",
+            longitude: "",
+            population: "",
+            wages: "",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount() {
-        axios.get("http://ctp-zip-api.herokuapp.com/zip/" + this.props.zipcode).then((response) => {
-            const data = response.data;
+    handleChange(event) {
+        this.setState({ zipCode: event.target.value });
+    }
 
-            console.log(data);
+    componentDidUpdate() {
+        if (this.state.zipCode.length !== 5) {
+            // do nothing when zipCode is not 5 characters long (for performance)
+        } else {
+            axios.get("http://ctp-zip-api.herokuapp.com/zip/" + this.state.zipCode).then((response) => {
+                const data = response.data;
 
-            const newZipSearchObj = {
-                stateName: data[0].State,
-                cityName: data[0].City,
-                locationText: data[0].LocationText,
-                latitude: data[0].Lat,
-                longitude: data[0].Long,
-                population: data[0].EstimatedPopulation,
-                wages: data[0].TotalWages,
-            };
+                console.log(data);
 
-            this.setState({ zip: newZipSearchObj });
-        }).catch((err) => console.log(err));
+                const newZipSearchObj = {
+                    stateName: data[0].State,
+                    cityName: data[0].City,
+                    locationText: data[0].LocationText,
+                    latitude: data[0].Lat,
+                    longitude: data[0].Long,
+                    population: data[0].EstimatedPopulation,
+                    wages: data[0].TotalWages,
+                };
 
+                this.setState(newZipSearchObj);
+            }).catch((err) => console.log(err));
+        }
     }
 
     render() {
-        let display;
-        if (!this.state.zip) {
-            display = <p>Not Found</p>;
-        } else {
-            display = (
+        return (
+            <>
+                <div className="zipcode">
+                    <h2>Zipcode</h2>
+                </div>
+                <input
+                    className="zipcode_input"
+                    type="text"
+                    name="zipcode"
+                    value={this.state.zipCode}
+                    placeholder="10310"
+                    onChange={this.handleChange.bind(this)}
+                />
+
                 <>
                     <h1>{this.state.locationText}</h1>
                     <ul>
-                        <li>State: {this.state.zip.stateName}</li>
-                        <li>Location: ({this.state.zip.latitude}, {this.state.zip.longitude})</li>
-                        <li>Population (estimated): {this.state.zip.population}</li>
-                        <li>Total Wages: {this.state.zip.wages}</li>
+                        <li>State: {this.state.stateName}</li>
+                        <li>Location: ({this.state.latitude}, {this.state.longitude})</li>
+                        <li>Population (estimated): {this.state.population}</li>
+                        <li>Total Wages: {this.state.wages}</li>
                     </ul>
                 </>
-            );
-        }
-        
-        return <div className="zip">{display}</div>;
+
+            </>
+        );
     }
 }
 
